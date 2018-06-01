c2dcf412c1c05b87904f8afd1279769f67065267
Kateryna Zhuzha <kathrin.krylova@gmail.com>
2018-06-01 11:08:12 +0200

    fix FindMinimum tests

--- TechnicalTestCore/Minimum.cs
+++ TechnicalTestCore/Minimum.cs
@@ -6,7 +6,15 @@
         public static int MinimumElementIn(List<int> intlist)
         {
             // Adapt the method body to make the corresponding unit test in 'TechnicalTestUnitTests' pass
-            return -1;
+            int minElement = intlist[0];
+            for (int i = 1; i < intlist.ToArray().Length; i++)
+            {
+                if (minElement > intlist[i])
+                {
+                    minElement = intlist[i];
+                }
+            }
+            return minElement;
         }
     }
 }
--- TechnicalTestCore/Summations.cs
+++ TechnicalTestCore/Summations.cs
@@ -5,13 +5,24 @@
         public static int SumOfNumbersFrom1TillNrAsInt(int maxNrAsInt)
         {
             // Adapt the method body to make the corresponding unit test in 'TechnicalTestUnitTests' pass
-            return -1;
+            int sumOfNumbers = 0;
+            for (int i = 1; i <= maxNrAsInt; i++)
+            {
+                sumOfNumbers = sumOfNumbers + i;
+            }
+            return sumOfNumbers;
         }
 
         public static string SumOfNumbersFrom1TillNrAsString(string maxNrAsString)
         {
             // Adapt the method body to make the corresponding unit test in 'TechnicalTestUnitTests' pass
-            return "-1";
+            uint maxNrAsInt = uint.Parse(maxNrAsString);
+            uint sumOfNumbers = 0;
+            for (uint i = 1; i <= maxNrAsInt; i++)
+            {
+                sumOfNumbers = sumOfNumbers + i;
+            }
+            return sumOfNumbers.ToString();            
         }
     }
 }
