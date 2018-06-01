e576853237533810d875e14abc0d6a1d380869d6
Kateryna Zhuzha <kathrin.krylova@gmail.com>
2018-06-01 11:27:33 +0200

    fix HelloWorld test

--- TechnicalTestCore/HelloWorld.cs
+++ TechnicalTestCore/HelloWorld.cs
@@ -4,7 +4,7 @@
     {
         public static void SayHelloToTheWorld(string[] args)
         {
-            // Write a method body which makes the corresponding unit test in the 'TechnicalTestUnitTests' project pass
+            System.Console.WriteLine("Hello World!");
         }
     }
 }
